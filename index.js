93f801090ff1e65c88a2a91461ff98a018466d56
rakibul-cse-diu <rakibul2180@gmail.com>
2022-05-25 14:38:14 +0600

    connect to the mmongodb database

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express')
-const app = express()
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
+
+const app = express();
+require('dotenv').config();
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@node-mongo-server-1.pkxfn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        console.log("db connected");
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello World...!')
